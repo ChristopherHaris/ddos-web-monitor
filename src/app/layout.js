36c2075ec3924c665e97bcb823e09d1ff5ba508c
Christopher Haris <christoharis2005@gmail.com>
2023-11-13 16:10:29 +0700

    adding theme button

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import "./globals.css";
 import { Inter } from "next/font/google";
@@ -7,7 +7,9 @@ import Image from "next/image";
 import { MainNav } from "@/components/dashboard/main-nav";
 import { UserNav } from "@/components/dashboard/user-nav";
 import { useRouter } from "next/navigation";
+import { ThemeProvider } from "@/components/theme/theme-provider";
 import { useEffect } from "react";
+import { ModeToggle } from "@/components/theme/theme-buttom";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,21 +24,29 @@ export default function RootLayout({ children }) {
   //     router.push("/authentication/login");
   //   }
   // }, []);
-  
+
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="flex-col flex">
-          <div className="border-b">
-            <div className="flex h-16 items-center px-4">
-              <MainNav className="mx-6" />
-              <div className="ml-auto flex items-center space-x-4">
-                <UserNav />
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className="flex-col flex">
+            <div className="border-b">
+              <div className="flex h-16 items-center px-4">
+                <MainNav className="mx-6" />
+                <div className="ml-auto flex items-center space-x-4">
+                  <ModeToggle />
+                  <UserNav />
+                </div>
               </div>
             </div>
+            {children}
           </div>
-          {children}
-        </div>
+        </ThemeProvider>
       </body>
       {/* <Toaster /> */}
     </html>
